645dd7a0bbc20e5013990e488ec9ef71a2f421c5
walshjayson720-rgbrayloguss <walshjayson720@gmail.com>
2026-01-19 14:52:17 -0500

    Create tiles.js

---
+++ world/tiles.js
@@ -0,0 +1,7 @@
+const TILE_SIZE = 8;
+
+const TILES = {
+  grass: { color: "#3fa34d", walkable: true },
+  water: { color: "#2b6cb0", walkable: false },
+  sand:  { color: "#d1b45a", walkable: true }
+};
